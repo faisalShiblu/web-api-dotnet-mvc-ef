c4ef97c937178a2aa9a3696ed52793a0e5abbffb
Shiblu <faisalalamshiblu@gmail.com>
2019-06-03 16:25:17 +0600

    first commit

---
+++ EmployeeRegistration/dotNETMVCClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dotNETMVCClient.Startup))]
+namespace dotNETMVCClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
